64524700bd24a4efb21d6b88f62c94f4ef06c048
ParenkovLocal <parenkov.fedor@gmail.com>
2021-09-20 16:01:41 +0400

    Add selenide listener

--- src/test/java/parenkov/allure/GithubTest.java
+++ src/test/java/parenkov/allure/GithubTest.java
@@ -1,5 +1,7 @@
 package parenkov.allure;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -11,13 +13,17 @@ public class GithubTest {
 
     @Test
     public void githubTest() {
-        open("https://github.com");
-        $(".header-search-input").sendKeys("selenide");
-        $(".header-search-input").submit();
 
-        $(By.linkText("selenide/selenide")).click();
-        $("#issues-tab").click();
-        $("#issue_1543_link").shouldHave(text("Please add stack trace to every error in SoftAsserts"));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+        $(".header-search-input").as("Поисковая строка").sendKeys("selenide");
+        $(".header-search-input").as("Поисковая строка").submit();
+
+        $(By.linkText("selenide/selenide")).as("Ссылка для перехода в репозиторий проекта Selenide").click();
+        $("#issues-tab").as("Ссылка для перехода в раздел Issues").click();
+        $("#issue_1543_link").as("Сcылка на issue с #1543")
+                .shouldHave(text("Please add stack trace to every error in SoftAsserts"));
 
     }
 }
